ad85c081da6ec75fdd84bb569d33da4225b3bc77
Giuliano Gusmao <giulianoggg@hotmail.com>
2022-06-08 09:33:38 -0300

    somente query

--- GetProductNameHacker3/index.js
+++ GetProductNameHacker3/index.js
@@ -1,9 +1,9 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const productId = (req.query.productId || (req.body && req.body.productId));
+    const productId = (req.query.productId);
     const responseMessage = productId
-        ? `The product name for your product id ${productId} is Starfruit Explosion and the description is This starfruit ice cream is out of this world!`
+        ? `The product name for your product id ${productId} is Starfruit Explosion`
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
